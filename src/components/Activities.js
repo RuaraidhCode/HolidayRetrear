b060d6acc235446509f21c0fb1b8eec3b3f59ad7
Ruaraidh <postgres@ruaraidhslaptop.home>
2024-01-08 22:37:51 +0000

    Update image paths for GitHub Pages

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -5,32 +5,32 @@ const activitiesData = [
   {
     name: "Games Room",
     description: " A pool table which turns into a table tennis table. A PS4 with a collection of games",
-    imageUrl: "/improved-images/balgonie(13).jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/improved-images/balgonie(13).jpeg", // Replace with actual image path
   },
   {
     name: "Sauna",
     description: "Relax in a cozy sauna after a long day.",
-    imageUrl: "/improved-images/sauna.jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/improved-images/sauna.jpeg", // Replace with actual image path
   },
   {
     name: "Snug Lounge",
     description: "A cozy place to relax with a Smart TV, board games, piano and guitars",
-    imageUrl: "/improved-images/balgonie(8).jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/improved-images/balgonie(8).jpeg", // Replace with actual image path
   },
   {
     name: "Garden",
     description: "Large mature garden with patio seating area, and covered decked area with seating and charcoal BBQ",
-    imageUrl: "/images/processed (7).jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/images/processed (7).jpeg", // Replace with actual image path
   },
   {
     name: "5 Bedrooms",
     description: "2 Super king size bedrooms, 2 twins and 1 single",
-    imageUrl: "/improved-images/balgonie(9).jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/improved-images/balgonie(9).jpeg", // Replace with actual image path
   },
   {
     name: "2 Wood Burners",
     description: "2 big baskets of logs provided",
-    imageUrl: "/improved-images/balgonie(3).jpeg", // Replace with actual image path
+    imageUrl: process.env.PUBLIC_URL + "/improved-images/balgonie(3).jpeg", // Replace with actual image path
   },
   // Add more activities as needed
 ];
